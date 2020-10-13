d63d349694651a7ac267d577747cb051468e7989
Akatsuki1910 <akatsukicirno@gmail.com>
2020-10-13 21:01:18 +0900

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,31 +1,37 @@
 <template>
-  <div class="footer">
-		<div class="title">Footer</div>
-  </div>
+	<div class="footer">
+		<div class="box">
+			<div class="copyright">©奈良高専情報工学科</div>
+			<a href="https://www.nara-k.ac.jp/">奈良高専</a>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'Footer'
-}
+	export default {
+		name: 'Footer'
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='scss' scoped>
-.footer{
-	width: 100%;
-	height: 10vh;
-	position: absolute;
-	bottom: 0;
-	background-color: skyblue;
-}
+	.footer {
+		width: 100%;
+		height: 10vh;
+		position: absolute;
+		bottom: 0;
+		background-color: skyblue;
+		text-align: center;
+	}
 
-.title{
-	position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translateY(-50%) translateX(-50%);
+	.box {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translateY(-50%) translateX(-50%);
+	}
 
-	font-size: 20px;
-}
+	.copyright {
+		font-size: 20px;
+	}
 </style>
