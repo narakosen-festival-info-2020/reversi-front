05d97a1c804417bb5ebfe71ce08779de2d6ee28d
さりえり <56764525+salieri256@users.noreply.github.com>
2020-10-23 12:34:12 +0900

    一旦

--- pages/index.vue
+++ pages/index.vue
@@ -4,15 +4,17 @@
       <div class="select-mode">
         モードを選んでください
       </div>
-      <button class="center" @click="gameStart">
-        Game Start
-      </button>
-      <button class="center" @click="gameRandom">
-        Rondom Game Start
-      </button>
-      <button class="center" @click="gameCustom">
-        Custom Game
-      </button>
+      <div class="btn-wrapper">
+        <button class="center mode-btn" @click="gameStart">
+          Game Start
+        </button>
+        <button class="center mode-btn" @click="gameRandom">
+          Rondom Game Start
+        </button>
+        <button class="center mode-btn" @click="gameCustom">
+          Custom Game
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -38,8 +40,22 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+.main {
+  background-color: #02af44;
+  background-image: repeating-linear-gradient(
+    -45deg,
+    transparent, transparent 28px,
+    #393939, #393939 30px,
+  ), repeating-linear-gradient(
+    45deg,
+    transparent, transparent 28px,
+    #393939, #393939 30px,
+  );
+  animation: move 1s linear infinite;/*左と上がきれいにつながらない...*/
+}
 .select-mode{
   font-size: 2rem;
+  color: white;
 }
 .box{
   width: 100%;
@@ -48,5 +64,30 @@ export default {
   left:50%;
   transform: translateX(-50%) translateY(-50%);
   text-align: center;
+}
+.btn-wrapper {
+  width: 30%;
+  min-width: 150px;
+  margin: auto;
+}
+.mode-btn {
+  width: 100%;
+  margin: 10px 0px;
+  padding: 10px;
+  border: 5px solid #191919;
+  border-radius: 6px;
+  color: white;
+  background-color: #292929;
+  /*ストライプを作る*/
+  background-image: repeating-linear-gradient(
+    -45deg,
+    transparent, transparent 5px,
+    #393939, #393939 8px
+  );
+}
+
+@keyframes move {
+  0% {background-position: 0px 0px;}
+  100% {background-position: 23px 23px;}
 }
 </style>
