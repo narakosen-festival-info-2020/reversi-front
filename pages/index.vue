b36b3f4439bf6b57bc446782d8def49a65f3571a
akatsuki1910 <akatsukicirno@gmail.com>
2020-10-27 23:14:39 +0900

    update
    cssを書き換え

--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,13 @@
         モードを選んでください
       </div>
       <div class="btn-wrapper">
-        <button class="center mode-btn btn-black" @click="gameStart">
+        <button class="btn-black" @click="gameStart">
           Game Start
         </button>
-        <button class="center mode-btn btn-black" @click="gameCircle">
+        <button class="btn-black" @click="gameCircle">
           Circle Game Start
         </button>
-        <button class="center mode-btn btn-white" @click="gameCustom">
+        <button class="btn-white" @click="gameCustom">
           Custom Game
         </button>
       </div>
@@ -40,38 +40,7 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-.main {
-  background-color: #ee7800;
-  background-image: repeating-linear-gradient(
-    -45deg,
-    transparent, transparent 28px,
-    #fffbec, #fffbec 30px,
-  ), repeating-linear-gradient(
-    45deg,
-    transparent, transparent 28px,
-    #fffef6, #fffef6 30px,
-  );
-  background-size: calc(100% + 50px) calc(100% + 50px);/* 移動させる分だけサイズを大きくしておく(50pxは大体) */
-  animation: move 1.5s linear infinite;
-}
-.select-mode{
-  font-size: 2rem;
-  color: black;
 
-  font-family: vdl-logojrblack, sans-serif;
-  font-style: normal;
-  font-weight: 900;
-
-  /* 2pxふちどり */
-  text-shadow: white 2px 0px,  white -2px 0px,
-    white 0px -2px, white 0px 2px,
-    white 2px 2px , white -2px 2px,
-    white 2px -2px, white -2px -2px,
-    white 1px 2px,  white -1px 2px,
-    white 1px -2px, white -1px -2px,
-    white 2px 1px,  white -2px 1px,
-    white 2px -1px, white -2px -1px;
-}
 .box{
   width: 100%;
   position: absolute;
@@ -79,49 +48,24 @@ export default {
   left:50%;
   transform: translateX(-50%) translateY(-50%);
   text-align: center;
-}
-.btn-wrapper {
-  width: 30%;
-  min-width: 150px;
-  margin: auto;
-}
-.mode-btn {
-  width: 100%;
-  margin: 10px 0px;
-  padding: 10px;
-  border-radius: 6px;
-}
-
-.btn-black {
-  border: 5px solid #191919;
-  color: white;
-  background-color: #292929;
-  /*ストライプを作る*/
-  /*
-  background-image: repeating-linear-gradient(
-    -45deg,
-    transparent, transparent 5px,
-    #393939, #393939 8px
-  );
-  */
-}
-
-.btn-white {
-  border: 5px solid #e9e9e9;
-  color: black;
-  background-color: #d9d9d9;
-  /*ストライプを作る*/
-  /*
-  background-image: repeating-linear-gradient(
-    -45deg,
-    transparent, transparent 5px,
-    #c9c9c9, #c9c9c9 8px
-  );
-  */
-}
-
-@keyframes move {
-  0% {background-position: -42px -42px;}
-  100% {background-position: 0px 0px;}
+  .select-mode{
+    font-size: 2rem;
+    @include sp-ip5 {
+      font-size: 1.7rem;
+    }
+    color: black;
+    font-family: vdl-logojrblack, sans-serif;
+    font-style: normal;
+    font-weight: 900;
+    text-shadow:
+      2px  2px 0px #ffffff,
+      -2px  2px 0px #ffffff,
+      2px -2px 0px #ffffff,
+      -2px -2px 0px #ffffff,
+      2px  0px 0px #ffffff,
+      0px  2px 0px #ffffff,
+      -2px  0px 0px #ffffff,
+      0px -2px 0px #ffffff;
+  }
 }
 </style>
