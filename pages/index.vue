c39fcefba4471a46b5f7cb2e48c8058edb5ad5cd
さりえり <nakakou201250@gmail.com>
2020-10-27 16:18:51 +0900

    upload index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -8,8 +8,8 @@
         <button class="center mode-btn btn-black" @click="gameStart">
           Game Start
         </button>
-        <button class="center mode-btn btn-black" @click="gameCircle">
-          Circle Game Start
+        <button class="center mode-btn btn-black" @click="gameRandom">
+          Rondom Game Start
         </button>
         <button class="center mode-btn btn-white" @click="gameCustom">
           Custom Game
@@ -26,8 +26,8 @@ export default {
       this.$store.commit('set', 'normal')
       this.$router.push('/Game')
     },
-    gameCircle () {
-      this.$store.commit('set', 'circle')
+    gameRandom () {
+      this.$store.commit('set', 'random')
       this.$router.push('/Game')
     },
     gameCustom () {
@@ -41,21 +41,36 @@ export default {
 
 <style lang='scss' scoped>
 .main {
-  background-color: #02af44;
+  background-color: #ee7800;
   background-image: repeating-linear-gradient(
     -45deg,
     transparent, transparent 28px,
-    #393939, #393939 30px,
+    #fffbec, #fffbec 30px,
   ), repeating-linear-gradient(
     45deg,
     transparent, transparent 28px,
-    #393939, #393939 30px,
+    #fffef6, #fffef6 30px,
   );
-  animation: move 1s linear infinite;/*左と上がきれいにつながらない...*/
+  background-size: calc(100% + 50px) calc(100% + 50px);/* 移動させる分だけサイズを大きくしておく(50pxは大体) */
+  animation: move 1.5s linear infinite;
 }
 .select-mode{
   font-size: 2rem;
-  color: white;
+  color: black;
+
+  font-family: vdl-logojrblack, sans-serif;
+  font-style: normal;
+  font-weight: 900;
+
+  /* 2pxふちどり */
+  text-shadow: white 2px 0px,  white -2px 0px,
+    white 0px -2px, white 0px 2px,
+    white 2px 2px , white -2px 2px,
+    white 2px -2px, white -2px -2px,
+    white 1px 2px,  white -1px 2px,
+    white 1px -2px, white -1px -2px,
+    white 2px 1px,  white -2px 1px,
+    white 2px -1px, white -2px -1px;
 }
 .box{
   width: 100%;
@@ -82,11 +97,13 @@ export default {
   color: white;
   background-color: #292929;
   /*ストライプを作る*/
+  /*
   background-image: repeating-linear-gradient(
     -45deg,
     transparent, transparent 5px,
     #393939, #393939 8px
   );
+  */
 }
 
 .btn-white {
@@ -94,15 +111,17 @@ export default {
   color: black;
   background-color: #d9d9d9;
   /*ストライプを作る*/
+  /*
   background-image: repeating-linear-gradient(
     -45deg,
     transparent, transparent 5px,
     #c9c9c9, #c9c9c9 8px
   );
+  */
 }
 
 @keyframes move {
-  0% {background-position: 0px 0px;}
-  100% {background-position: 23px 23px;}
+  0% {background-position: -42px -42px;}
+  100% {background-position: 0px 0px;}
 }
 </style>
