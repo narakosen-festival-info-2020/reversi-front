dee6bdcf2078d315d689a5a2cc705dab0a9c64ad
akatsuki1910 <akatsukicirno@gmail.com>
2020-11-03 16:35:38 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,15 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://mplus-fonts.sourceforge.jp/webfonts/basic_latin/mplus_webfonts.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://mplus-fonts.sourceforge.jp/webfonts/general-j/mplus_webfonts.css'
+      }
     ]
   },
 
